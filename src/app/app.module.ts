b05c0655bf2c180651b790d74c42d8a83c811891
khq1 <74719753+khq1@users.noreply.github.com>
2022-04-27 12:32:38 +0200

    Toolbar component in css
    add style.css config 0 margin, app-component toolbar display,

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent
   ],
   imports: [
     BrowserModule,
