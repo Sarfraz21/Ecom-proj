6f43f2e19df951a613cd0798ee0e87ca465203f3
khq1 <74719753+khq1@users.noreply.github.com>
2022-05-17 19:37:01 +0200

    Toolbar style, add "Roboto" font import, routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { MainComponent } from './components/main/main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ToolbarComponent
+    ToolbarComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
